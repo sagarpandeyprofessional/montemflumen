f9d0d7ce35639627bf30101061ed4fa4540664a0
Roberto Herrera <betohr48@gmail.com>
2024-04-07 04:50:47 -0600

    Fisrt version

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,42 +1,74 @@
+
 const company_name = "Montem Flumen Construction and Robotics";
+const tag_line = "Build Smarter";
+const description = "Montem Flumen Construction and Robotics leads innovation in construction, prioritizing safety and efficiency through advanced drone and robot technology. Our startup company mission: automate processes, reduce manpower, and expedite timelines. Ideal for countries with smaller workforces, our solutions revolutionize construction for a safer, faster, and more technologically advanced future."
+const about_us = "We started this startup company to bring like-minded people together to create a safer construction environment. We believe that the future of construction is in automation and robotics. Our team is dedicated to creating innovative solutions that will revolutionize the industry. We are committed to making construction safer, faster, and more efficient. Our goal is to provide the best possible service to our customers and to help them achieve their construction goals. We are passionate about what we do and we are excited to be a part of the future of construction."
+const mission_short = "To create safer construction work environments worldwide by harnessing the power of drone technology, thereby preventing accidents and supporting countries with limited workforces.";
+const mission_long = "";
+const vision_short = "Our vision is to transform construction with drone solutions that enhance safety, optimize workflows, and minimize accidents, thereby fostering safer, sustainable infrastructure.";
+const vision_long = "";
+
+const social_links = [
+    {
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/company/montem-flumen-construction-and-robotics",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-5 h-5" viewBox="0 0 16 16">
+        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+    </svg>`
+    },
+    {
+        name: "Phone",
+        link: "tel:+821096959805",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"  viewBox="2 -5 40 40"><path d="M23.45 20.93a3 3 0 0 0-4.25 0l-1.45 1.45a26.14 26.14 0 0 1-4.51-3.62 26.62 26.62 0 0 1-3.62-4.51l1.45-1.45a3 3 0 0 0 0-4.25L8.24 5.73a2.93 2.93 0 0 0-2.11-.88h0A3 3 0 0 0 4 5.73L2.64 7.08A6.37 6.37 0 0 0 1 12.33c.32 3.75 2.78 8.26 6.57 12.06S15.92 30.64 19.67 31a7.87 7.87 0 0 0 .84 0 6.07 6.07 0 0 0 4.41-1.64L26.27 28a3 3 0 0 0 .88-2.13 2.93 2.93 0 0 0-.88-2.11zm1.41 5.66L23.5 27.94a4.57 4.57 0 0 1-3.66 1c-3.25-.28-7.39-2.58-10.81-6S3.31 15.41 3 12.16a4.53 4.53 0 0 1 1-3.66L5.41 7.14a1 1 0 0 1 .71-.29h0a1 1 0 0 1 .71.29L9.66 10a1 1 0 0 1 0 1.41l-2 2a1 1 0 0 0-.16 1.21 27.49 27.49 0 0 0 4.33 5.58 27.49 27.49 0 0 0 5.58 4.33 1 1 0 0 0 1.21-.16l2-2a1 1 0 0 1 1.41 0l2.83 2.83h0a1 1 0 0 1 .29.71A1 1 0 0 1 24.86 26.59zM26.6 5.39A14.92 14.92 0 0 0 16 1a1 1 0 1 0 0 2A13 13 0 0 1 29 16.11a1 1 0 0 0 1 1h0a1 1 0 0 0 1-1A14.91 14.91 0 0 0 26.6 5.39z"></path><path d="M20.91,11.11a6,6,0,0,1,1.77,4.31,1,1,0,0,0,1,1h0a1,1,0,0,0,1-1,8,8,0,0,0-8-8.1,1,1,0,1,0,0,2A6,6,0,0,1,20.91,11.11Z"></path></svg>`    
+    },
+]
 
 const services = [
     {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+        title: "Painting Drones",
+        description: "Our painting drones provide a revolutionary solution to streamline and enhance the painting process in construction. The value proposition lies in their ability to significantly reduce the risk and time associated with manual painting, all while ensuring precision and uniformity.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /></svg>`
+    },
+    {
+        title: "Window-Cleaning Drones",
+        description: " There's a huge demand for the window cleaning system for skyscrapers or other tall buildings. Using the same attitude as for a painting, we provide a convenient and safe solution to clean windows at any height. It's way more quickly and cheaper than traditional manual cleaning.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
+        title: "Welding Drones",
+        description: "Welding is often used for metal parts connection in the skyscrapers and bridges. Drone's application for the same tasks allows for increased work-done speed and prevents a lot of death cases by removing people from dangerous places. The value proposition here is first of all safety, and the ability to use welding for the hard-to-reach areas.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /></svg>`
+    },
+    {
+        title: "Scanning Drones",
+        description: "Nowadays every building is created in BIM(Building Information Model), but it's also could be useful to see historical and old buildings' informational models for monitoring conditions.Our scanning drones offer a cutting-edge solution for comprehensive site analysis and data collection.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" /></svg>`
+    },
+    {
+        title: "Window-Installing Drones",
+        description: "Our window-installing drones redefine the installation process, combining speed and precision. The value proposition is centered on the swift and accurate placement of windows in various construction scenarios, reducing labor costs and increasing overall project efficiency. These drones offer a safer and more efficient alternative to traditional installation methods.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+        title: "Dexterous Drones",
+        description: "Montem Flumen's drones with hands introduce a new level of versatility to construction tasks. The value proposition lies in their ability to perform intricate, hands-on operations, such as manipulating objects and assembling components. These drones enhance efficiency by minimizing the need for manual labor in tasks that demand precision.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+        title: "Transport Drones",
+        description: "Our carry-drones offer a logistical solution for transporting construction materials and equipment. The value proposition includes the efficient and timely movement of goods across construction sites, reducing the reliance on manual labor for material handling. This leads to cost savings, improved productivity, and enhanced safety.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
+        title: "Exoskeletons",
+        description: "Montem Flumen's exoskeletons provide wearable technology to enhance the physical capabilities of construction workers. The value proposition is the reduction of physical strain and fatigue, leading to increased worker productivity, improved safety, and a more comfortable working environment.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
-    },
-    {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
+    }
 ];
 
-export { services, company_name };
+export { services, company_name, tag_line, description, mission_short, mission_long, vision_short, vision_long, about_us, social_links };
