75f814d9a89c0f4860095f54347dc1e4a0996ca0
John Paul Ernersto Paredes Diez de Medina <125041352+JohnPaul0403@users.noreply.github.com>
2024-06-05 20:31:49 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
+import awsAmplify from 'astro-aws-amplify'; 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: awsAmplify(),
 });
